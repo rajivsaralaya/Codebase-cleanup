7127fa57dc37849266166ff62d9d3664eccdedc8
rajivsaralaya <104799236+rajivsaralaya@users.noreply.github.com>
2022-05-14 16:03:13 -0400

    Update Test_Utils.py

--- Tests/Test_Utils.py
+++ Tests/Test_Utils.py
@@ -7,9 +7,9 @@ from app.utils import to_usd
     
     
     
-    def test_to_usd():
+def test_to_usd():
         # rounding to 2 decimals and have dollar sighn
-        assert to_usd(0.45555) == "$0.46"
+    assert to_usd(0.45555) == "$0.46"
         
         #large numbers should have comma as separator
-        assert to_usd(123456789.98) == "$123,456,789.98"
+    assert to_usd(123456789.98) == "$123,456,789.98"
