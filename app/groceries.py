798e042c9cc8780369adf3942bca3dfb30e8ef62
rajivsaralaya <104799236+rajivsaralaya@users.noreply.github.com>
2022-05-14 12:09:12 -0400

    Refactor Across Files

--- app/groceries.py
+++ app/groceries.py
@@ -7,18 +7,8 @@
 
 import os
 
-def to_usd(my_price):
-    """
-    This is a docstring. It tells us what this function is about.
-    What its responisibilities are.
-    What the params are about.
-    What datatypes the params are.
-    What this function will return.
-    Example of invoking the function.
 
-    Invole like this: to_usd(9.9999)
-    """
-    return('${:,.2f}'.format(p[my_price]))
+from app.utils import to_usd
 
 
 
